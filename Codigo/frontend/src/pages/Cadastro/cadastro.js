8760e6949d42465c4637a8a941d84b8376b3a085
fcoutoPuc <103599709+fcoutoPuc@users.noreply.github.com>
2023-03-31 17:46:01 -0300

    call backend to create a new user

--- Codigo/frontend/src/pages/Cadastro/cadastro.js
+++ Codigo/frontend/src/pages/Cadastro/cadastro.js
@@ -3,28 +3,62 @@ import './cadastro.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import financeLabLogo from '../../img/FinanceLabLogo.png'
 import imgCadastro from '../../img//CadastroImgs/cadastro.jfif'
-
-import { Link } from "react-router-dom";
+import api from '../../services/api'
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Cadastro() {
+    const [email, setEmail] = useState('');
+    const [nome, setNome] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [senha, setSenha] = useState('');
+    const [display, setDisplay] = useState('none')
+    const navigate = useNavigate()
+
+
+    async function handleCadastro(event) {
+        event.preventDefault();
+        try {
+            console.log('oi');
+            await api.post('usuario', { email, nome, telefone, senha });
+            setDisplay(display === 'visible')
+            setTimeout(() => {
+                navigate('/login')
+              }, 1000); // 2000 milissegundos = 2 segundos
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div>
 
             <Link to="/" class="titulo" href="index">
                 <img width="300px" src={financeLabLogo} alt="Logo" />
             </Link>
+            <div class="alert alert-success" role="alert" style={{ display: display }}>
+                        Usuário cadastrado com sucesso
+                    </div>
             <div class="img-main">
                 <img class="imagem" src={imgCadastro} alt="" />
             </div>
+
             <div class="container">
                 <div class="row">
-                    <form class="col-sm-12 col-md-6 col-lg-3">
-                    <h1 class="text-center">Cadastro</h1>
+                    
+                    <form class="col-sm-12 col-md-6 col-lg-3" onSubmit={handleCadastro}>
+                        <h1 class="text-center">Cadastro</h1>
                         {/* <!-- Name input --> */}
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input type="text" id="form3Example1c" class="form-control" />
+                                <input
+                                    value={nome}
+                                    onChange={event => setNome(event.target.value)}
+                                    type="text"
+                                    id="form3Example1c"
+                                    class="form-control"
+                                />
                                 <label class="form-label" for="form3Example1c">Nome</label>
                             </div>
                         </div>
@@ -33,8 +67,30 @@ export default function Cadastro() {
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input type="email" id="form3Example3c" class="form-control" />
+                                <input
+                                    type="email"
+                                    id="form3Example3c"
+                                    class="form-control"
+                                    value={email}
+                                    onChange={event => setEmail(event.target.value)}
+                                />
                                 <label class="form-label" for="form3Example3c">Email</label>
+                            </div>
+                        </div>
+
+
+                        {/* <!-- Telefone input --> */}
+                        <div class="d-flex flex-row align-items-center mb-4">
+                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                            <div class="form-outline flex-fill mb-0">
+                                <input
+                                    type="text"
+                                    id="form3Example3c"
+                                    class="form-control"
+                                    value={telefone}
+                                    onChange={event => setTelefone(event.target.value)}
+                                />
+                                <label class="form-label" for="form3Example3c">Telefone</label>
                             </div>
                         </div>
 
@@ -43,14 +99,20 @@ export default function Cadastro() {
                         <div class="d-flex flex-row align-items-center mb-4">
                             <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
                             <div class="form-outline flex-fill mb-0">
-                                <input type="password" id="form3Example4c" class="form-control" />
+                                <input
+                                    type="password"
+                                    id="form3Example4c"
+                                    class="form-control"
+                                    value={senha}
+                                    onChange={event => setSenha(event.target.value)}
+                                />
                                 <label class="form-label" for="form3Example4c">Senha</label>
                             </div>
                         </div>
 
                         {/* <!-- Register buttons --> */}
                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                            <button type="button" class="btn btn-primary btn-lg" onclick="cadastrar()">Registrar</button>
+                            <button type="submit" class="btn btn-primary btn-lg" >Registrar</button>
                         </div>
                     </form>
                 </div>
